7ec319842505d871c17c0f96d83a9f050fb3281d
Tom Banister <tgbanister@gmail.com>
2018-04-29 16:31:58 +0100

    added plugin to config

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,15 @@ module.exports = {
   siteMetadata: {
     title: 'Agile Links',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages'
+      }
+    }
+  ],
   pathPrefix: 'agile-static'
 }
