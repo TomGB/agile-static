3b58231a7be8a247b50ee0ea564f367eaf61dff2
TomGB <tgbanister@gmail.com>
2019-11-01 16:57:23 +0000

    removed-link

--- src/components/info.js
+++ src/components/info.js
@@ -4,7 +4,7 @@ const Info = (searchResults = []) => (
   <aside>
     <h2>What's this?</h2>
     <p>
-      The <a href='https://twitter.com/GDSteam'>GDS agile delivery community</a> added hundreds of links about agile, lean and people to a now defunct <a href='http://linkydink.io/groups/gds-agile-delivery-community'>Linkydink</a>. You can browse most of those links here.
+      The <a href='https://twitter.com/GDSteam'>GDS agile delivery community</a> added hundreds of links about agile, lean and people to a now defunct "Linkydink". You can browse most of those links here.
     </p>
     <p>
       They were recovered by scraping the <a href='https://twitter.com/agilereading'>companion Twitter feed</a> and then linking to the top 3 URLs returned when searching Google for the article's title.
